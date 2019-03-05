2a581a142ed75335362b979806efbb2accddb90e
Apogee56 <childersl16@students.ecu.edu>
2019-03-04 23:24:20 -0500

    Implemented proximityIntersect
    proxIntersect added
    New test batch created

--- src/posInd.py
+++ src/posInd.py
@@ -34,6 +34,44 @@ def printPositionalArray(posInd): #dic to be printed on std output and n is the 
 			print('{:<25s}'.format(''), '{:<10s}'.format(file), posInd[word][file],'\n')
 
 		print()
+
+
+def proxIntersect(p1,p2,k): #implements the proximity query algorithm provided in figure 2.12, page 39 of our book.
+        #The function returns a dictionary with documents as keys and positional pairs as values.
+        #The function takes a term p1, a term p2, and a distance k that must be satisfied.
+
+        #This implementation is essentially faithful to the psuedocode except that it returns the output in a slightly different format.
+
+        len_p1 = len(posInd[p2])
+        len_p2 = len(posInd[p2])
+        result = {}
+        docs = []
+        
+        for i in posInd[p1].keys():
+                for j in posInd[p2].keys():
+                        if (i == j):
+                                docs.append(i)
+
+        #print(posInd[p1].keys())
+        #print(docs)
+
+        for doc in docs:
+                l1 = posInd[p1][doc]
+                l2 = posInd[p2][doc]
+                l = []
+                for pos1 in l1:
+                        for pos2 in l2:
+                                if abs(pos1-pos2) <= k:
+                                        l.append(pos2)
+                                elif pos2 > pos1:
+                                        break
+                        while ((len(l) != 0) and (l[0] - pos1 > k)):
+                               l.pop(0)
+                        result.setdefault(doc, [])
+                        for position2 in l:
+                                result[doc].append((pos1,position2))
+        return result
+
 posInd = {}
 
 	# make a dictionary with a dictionary in it. the dict( key, dict2). dict2 key will be the file number and the key will be the list in there.
@@ -52,6 +90,8 @@ for x in filenames: #for each file
 			posInd[word][x].append(index)
 			
 printPositionalArray(posInd)
-		 
+#res = proxIntersect('this', 'hope', 1)
+#print(res);
+
 		
 		
